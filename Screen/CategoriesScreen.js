aabbc53ffffde9ad3d1597501f4c79baba13ffae
saifmogaadi12 <aboura.joujou@outlook.es>
2021-05-02 12:15:35 +0100

    Created a new Expo app

---
+++ Screen/CategoriesScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet,Text } from 'react-native'
+
+
+
+
+const CategoriesScreen = ()=>{
+    return (<Text></Text>)
+}
+
+
+
+
+
+const styles =StyleSheet.create({
+    screen : {
+        flex:1 , 
+        justifyContent:'center' , 
+        alignItems:'center'
+    }
+})
+export default Cat
